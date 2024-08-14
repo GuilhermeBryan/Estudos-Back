cd0f47df9ff7f8b15141690d35185ddee7af342a
Guilherme <guilhermebrya3@gmail.com>
2024-08-14 17:57:32 -0300

    att 3

---
+++ Banco de dados/na pratica.sql
@@ -0,0 +1,17 @@
+CREATE TABLE produto (
+	numeroidproduto integer not null,
+	nomeproduto varchar (50) not null,
+	cpfdocomprador char (11) not null,
+	nomedocomprador varchar (50) not null,
+	numero_de_compra integer not null,
+	complementos varchar,
+
+	--chave primária
+	constraint pk_pdt_id primary key (numeroidproduto)
+)
+
+
+insert into	produto (numeroidproduto, nomeproduto, cpfdocomprador, nomedocomprador, numero_de_compra, complementos)
+values
+	(2342, 'Action Figure', '87509235678', 'João Pedro Carvalho', 23043, null),
+	(6457, 'Gabinete New X PC', '986502139514', 'Gustavo Almeida', 32421, null);
