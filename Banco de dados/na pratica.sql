f975e58c73322b916e77b3c578e8d86a1aedf4f8
Guilherme <guilhermebrya3@gmail.com>
2024-08-20 15:34:34 -0300

    ATT 5

--- Banco de dados/na pratica.sql
+++ Banco de dados/na pratica.sql
@@ -11,8 +11,16 @@ CREATE TABLE produto  (
 )
 
 
-insert into	produto  (numeroidproduto, nomeproduto, cpfdocomprador, nomedocomprador, numero_de_compra, complementos)
+insert into	produto (numeroidproduto, nomeproduto, cpfdocomprador, nomedocomprador, numero_de_compra, complementos)
 values
 	(2342, 'Action Figure', '98247292012', 'João Pedro Carvalho', 23043, null),
-	(6457, 'Gabinete New X PC', '39430120592', 'Gustavo Almeida', 32421, null);
+	(6457, 'Gabinete New X PC', '39430120592', 'Gustavo Almeida', 32421, null),
+	(2344), 'Posters Marvel', '13409285932', 'Roberto Junqueira', 24543, null),
+	(4324), 'Maquiagens', '30219320120', 'Rosangela', 999, null),
+
+
+select nomeproduto, nomedocomprador from produto;
+select 'Nome do produto:' || nomeproduto || ' & Nome do comprador:' || nomedocomprador as "informações do produto" from produto;
+select * from produto limit 1; 
+select nomedocomprador from produto where nomedocomprador like 'G%';
 
