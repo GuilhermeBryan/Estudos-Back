46d6447c2f01a8d774c677aac85155342c56bcb5
Guilherme <guilhermebrya3@gmail.com>
2024-09-19 17:50:19 -0300

    ATT

--- Banco de dados/exercicios.sql
+++ Banco de dados/exercicios.sql
@@ -935,3 +935,65 @@ GROUP BY data_pedido
 
 
 --comandos adicionais
+
+--1.O nome do cliente e somente o mês de nascimento. Caso a data de nascimento não esteja preenchida mostrar a mensagem “Não informado”.
+SELECT * FROM tabelafinal
+SELECT nome, COALESCE(EXTRACT(MONTH FROM data_de_nascimento), 0) FROM tabelafinal
+--2.O nome do cliente e somente o nome do mês de nascimento (Janeiro, Fevereiro etc). Caso a data de nascimento não esteja preenchida mostrar a mensagem “Não informado")
+SELECT 
+	nome,
+	CASE EXTRACT(MONTH FROM data_de_nascimento) 
+	WHEN 1 Then 'Janeiro'
+	WHEN 2 THEN 'Fevereiro'
+	WHEN 3 THEN 'Março'
+	WHEN 4 THEN 'Abril'
+	WHEN 5 THEN 'Maio'
+	WHEN 6 THEN 'Junho'
+	WHEN 7 THEN 'Julho'
+	WHEN 8 THEN 'Agosto'
+	WHEN 9 THEN 'Setembro'
+	WHEN 10 THEN 'Outubro'
+	WHEN 11 THEN 'Novembro'
+	WHEN 12 THEN 'Dezembro'
+ELSE 'Não informado'
+END AS date
+FROM tabelafinal
+
+--3.O nome do cliente e somente o ano de nascimento. Caso a data de nascimento não esteja preenchida mostrar a mensagem “Não informado”.
+SELECT 
+	nome,
+	 COALESCE(EXTRACT(YEAR FROM data_de_nascimento), 0)
+FROM tabelafinal
+
+--4.O caractere 5 até o caractere 10 de todos os municípios.
+	SELECT
+		SUBSTRING(nomemunicipio FROM 1 FOR 5)
+	FROM municipio
+
+--5.O nome de todos os municípios em letras maiúsculas.
+ SELECT 
+ 	UPPER(nomemunicipio)
+FROM municipio
+--6.O nome do cliente e o gênero. Caso seja M mostrar “Masculino”, senão mostrar “Feminino”.
+SELECT 
+	nome,
+	CASE sexo
+	WHEN 'M' THEN 'Masculino'
+	WHEN 'F' THEN 'Feminino'
+END AS sex
+FROM tabelafinal
+--7.O nome do produto e o valor. Caso o valor seja maior do que R$ 500,00 mostrar a mensagem “Acima de 500”, caso contrário, mostrar a mensagem “Abaixo de 500”.
+SELECT * FROM produtos
+SELECT
+	nomeprodutos,
+	valor,
+CASE 
+	WHEN valor > 500 THEN 'Acima de 500'	
+	ELSE 'Abaixo de 500'
+END AS val
+FROM produtos
+
+
+--subconsultas
+--1. O nome dos clientes que moram na mesma cidade do Manoel. Não deve ser mostrado o Manoel.
+
